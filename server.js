f6c9bfb5edacdb7ea516a7be7ddb96da97e46544
santosh09099 <sanapalasantoshkumar26@gmail.com>
2024-07-31 22:13:58 +0530

    added

--- server.js
+++ server.js
@@ -125,6 +125,8 @@ io.on('connection', socket => {
     });
 });
 
+//server
+
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
